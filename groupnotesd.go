6a2e6567034c902c47eca1992511d252b37f74a3
rob <robdelacruz@gmail.com>
2019-12-31 09:47:40 +0800

    serve hello page

--- groupnotesd.go
+++ groupnotesd.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("test")
+	port := "8000"
+
+	http.HandleFunc("/", rootHandler())
+	fmt.Printf("Listening on %s...\n", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
+	log.Fatal(err)
+}
+
+func rootHandler() func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+
+		fmt.Fprintf(w, "hello.\n")
+	}
 }
