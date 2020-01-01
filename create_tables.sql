badff722a29bb6b1a9dae7ce5777ad0ab8e6871f
rob <robdelacruz@gmail.com>
2020-01-02 02:37:30 +0800

    fix formatting and implement note replies

--- create_tables.sql
+++ create_tables.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS note;
 CREATE TABLE note (note_id INTEGER PRIMARY KEY NOT NULL, title TEXT, body TEXT, createdt TEXT, user_id INTEGER, FOREIGN KEY(user_id) REFERENCES user);
 
 DROP TABLE IF EXISTS notereply;
-CREATE TABLE notereply (notereply_id INTEGER PRIMARY KEY NOT NULL, note_id INTEGER, body TEXT, createdt TEXT, user_id INTEGER, FOREIGN KEY(user_id) REFERENCES user, FOREIGN KEY(note_id) REFERENCES note);
+CREATE TABLE notereply (notereply_id INTEGER PRIMARY KEY NOT NULL, note_id INTEGER, replybody TEXT, createdt TEXT, user_id INTEGER, FOREIGN KEY(user_id) REFERENCES user, FOREIGN KEY(note_id) REFERENCES note);
 
 DROP TABLE IF EXISTS user;
 CREATE TABLE user (user_id INTEGER PRIMARY KEY NOT NULL, username TEXT);
--- groupnotesd.go
+++ groupnotesd.go
@@ -50,13 +50,10 @@ func notesHandler(db *sql.DB) func(http.ResponseWriter, *http.Request) {
 			var noteid int64
 			var title, body, createdt, username string
 			rows.Scan(&noteid, &title, &body, &createdt, &username)
-			tcreatedt, err := time.Parse(time.RFC3339, createdt)
-			if err != nil {
-				tcreatedt = time.Now()
-			}
+			tcreatedt, _ := time.Parse(time.RFC3339, createdt)
 
 			fmt.Fprintf(w, "<li>\n")
-			fmt.Fprintf(w, "<a href=\"/note/%d\">%s</a>\n", noteid, title)
+			fmt.Fprintf(w, "<a class=\"note-title\" href=\"/note/%d\">%s</a>\n", noteid, title)
 			printByline(w, username, tcreatedt)
 			fmt.Fprintf(w, "</li>\n")
 		}
@@ -72,18 +69,52 @@ func notesHandler(db *sql.DB) func(http.ResponseWriter, *http.Request) {
 	}
 }
 
+func parseNoteid(url string) int {
+	sre := `^/note/(\d+)$`
+	re := regexp.MustCompile(sre)
+	matches := re.FindStringSubmatch(url)
+	if matches == nil {
+		return -1
+	}
+	noteid, err := strconv.Atoi(matches[1])
+	if err != nil {
+		return -1
+	}
+	return noteid
+}
+
 func noteHandler(db *sql.DB) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		sre := `^/note/(\d+)$`
-		re := regexp.MustCompile(sre)
-		matches := re.FindStringSubmatch(r.URL.Path)
-		if matches == nil {
+		noteid := parseNoteid(r.URL.Path)
+		if noteid == -1 {
 			log.Printf("no noteid parameter in '%s'\n", r.URL.Path)
 			// no note id parameter, so redirect to notes list page.
 			http.Redirect(w, r, "/", http.StatusSeeOther)
 			return
 		}
-		noteid, _ := strconv.Atoi(matches[1])
+
+		if r.Method == "POST" {
+			replybody := r.FormValue("replybody")
+			createdt := time.Now().Format(time.RFC3339)
+			userid := 2
+
+			// Strip out linefeed chars so that CRLF becomes just CR.
+			// CRLF causes problems in markdown parsing.
+			replybody = strings.ReplaceAll(replybody, "\r", "")
+
+			s := "INSERT INTO notereply (note_id, replybody, createdt, user_id) VALUES (?, ?, ?, ?)"
+			stmt, err := db.Prepare(s)
+			if err != nil {
+				log.Fatal(err)
+			}
+			_, err = stmt.Exec(noteid, replybody, createdt, userid)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			http.Redirect(w, r, r.URL.Path, http.StatusSeeOther)
+			return
+		}
 
 		s := "SELECT title, body, createdt, username FROM note LEFT OUTER JOIN user ON note.user_id = user.user_id WHERE note_id = ? ORDER BY createdt DESC;"
 		row := db.QueryRow(s, noteid)
@@ -103,15 +134,14 @@ func noteHandler(db *sql.DB) func(http.ResponseWriter, *http.Request) {
 		printPageNav(w)
 
 		fmt.Fprintf(w, "<article>\n")
-		fmt.Fprintf(w, "<h1><a href=\"/note/%d\">%s</a></h1>", noteid, title)
+		fmt.Fprintf(w, "<h1><a class=\"note-title\" href=\"/note/%d\">%s</a></h1>", noteid, title)
 		tcreatedt, err := time.Parse(time.RFC3339, createdt)
 		if err != nil {
 			tcreatedt = time.Now()
 		}
 		printByline(w, username, tcreatedt)
 
-		//body = strings.ReplaceAll(body, "\r", "")
-		bodyMarkup := blackfriday.Run([]byte(body))
+		bodyMarkup := parseMarkdown(body)
 		fmt.Fprintf(w, string(bodyMarkup))
 		fmt.Fprintf(w, "</article>\n")
 
@@ -120,12 +150,31 @@ func noteHandler(db *sql.DB) func(http.ResponseWriter, *http.Request) {
 <p>Replies:</p>
 `)
 
-		fmt.Fprintf(w, "<p class=\"byline\">1. robdelacruz wrote on 2019-12-06:</p>")
-		fmt.Fprintf(w, "<p>comment text here</p>")
+		s = "SELECT replybody, createdt, username FROM notereply LEFT OUTER JOIN user ON notereply.user_id = user.user_id WHERE note_id = ? ORDER BY notereply_id"
+		rows, err := db.Query(s, noteid)
+		if err != nil {
+			fmt.Fprintf(w, "<p class=\"byline\">Error loading replies</p>")
+			fmt.Fprintf(w, "</article>\n")
+			printPageFoot(w)
+			log.Fatal(err)
+			return
+		}
+		i := 1
+		for rows.Next() {
+			var replybody, createdt, username string
+			rows.Scan(&replybody, &createdt, &username)
+			tcreatedt, _ := time.Parse(time.RFC3339, createdt)
+			createdt = tcreatedt.Format("2 Jan 2006")
 
-		fmt.Fprintf(w, `<form>
+			fmt.Fprintf(w, "<p class=\"byline\">%d. %s wrote on %s:</p>", i, username, createdt)
+			replybodyMarkup := parseMarkdown(replybody)
+			fmt.Fprintf(w, string(replybodyMarkup))
+			i++
+		}
+
+		fmt.Fprintf(w, `<form method="post">
         <p class="byline">post comment:</p>
-        <textarea rows="10" cols="80"></textarea><br>
+        <textarea name="replybody" rows="10" cols="80"></textarea><br>
         <button>add reply</button>
     </form>
 `)
@@ -213,4 +262,8 @@ func printPageNav(w io.Writer) {
     <p class="byline">I reserve the right to be biased, it makes life more interesting.</p>
 </nav>
 `)
+}
+
+func parseMarkdown(s string) string {
+	return string(blackfriday.Run([]byte(s), blackfriday.WithExtensions(blackfriday.HardLineBreak)))
 }
